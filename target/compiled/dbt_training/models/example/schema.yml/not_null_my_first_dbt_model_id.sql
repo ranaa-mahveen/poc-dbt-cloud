e7550d19a289fb5ffd3d6efd1c916ac60e467a0c
ranaa-mahveen <ranaa.mahveen@move.com>
2023-01-23 10:56:36 -0500

    add dbt files

---
+++ target/compiled/dbt_training/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from SNOWFLAKE.ACCOUNT_USAGE.my_first_dbt_model
+where id is null
+
+
---
+++ target/compiled/dbt_training/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from SNOWFLAKE.ACCOUNT_USAGE.my_second_dbt_model
+where id is null
+
+
